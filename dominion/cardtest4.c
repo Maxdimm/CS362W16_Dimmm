44254842a75267432a9fd11edca4126feb3a892b
Max Dimm <dimmm@oregonstate.edu>
2016-02-04 18:31:17 -0800

    Update cardtest4.c

--- dominion/cardtest4.c
+++ dominion/cardtest4.c
@@ -1,91 +1,151 @@
 #include "dominion.h"
 #include "dominion_helpers.h"
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <time.h>
 #include <assert.h>
 #include "rngs.h"
-#include <time.h>
 
-#define DEBUG 0
-#define NOISY_TEST 1
+// Tests the steward card in dominion.c
+int main (int argc, char** argv) {
+    printf("TESTING steward card\n");
 
-int main(){
     srand(time(NULL));
+
+    int numplayers = 2;
     struct gameState G;
-    int seed = 1000;
-    int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
-           sea_hag, tribute, smithy};
+    int k[10] = {adventurer, gardens, embargo, village, steward, mine, cutpurse, sea_hag, tribute, smithy};
 
-    printf("TESTING adventurer card\n");
     printf("RANDOM TESTS\n");
-    int i, j;
-    int randomNumber;
-    int numCopper = 0;
-    int numSilver = 0;
-    int numGold = 0;
-    //Loops through all four players for testing purposes
-    for(i = 0; i < 10; i++){
-        printf("\nTest number %d\n", i + 1);
-        //Initialize game with 4 players
-        initializeGame(2, k, seed, &G);
-        G.whoseTurn = 0;
 
-        //Put the adventurer card in the player's hands
-        G.hand[0][0] = adventurer;
-        G.handCount[0] = 1;
+    int numtests = 100;
+    for(int i = 0; i < numtests; i++){
+        initializeGame(numplayers, k, rand(), &G);
+          int money = 0;
+          int stewardPos = -1;
+          int adventurerPos = -1;
+          int i=0;
 
-        //Randomly generate cards in the deck
-        for(j = 0; j < 50; j++){
-            randomNumber = rand() % 50;
-            if(randomNumber >= 27 && randomNumber < 35)
-                G.deck[0][j] = 4;
-            else if(randomNumber >=35 && randomNumber < 42)
-                G.deck[0][j] = 5;
-            else if(randomNumber >=42 && randomNumber < 50)
-                G.deck[0][j] = 6;
-            else
-                G.deck[0][j] = randomNumber;
-        }
-        G.deckCount[0] = 50;
+          int numStewards = 0;
+          int numAdventurers = 0;
 
-        printf("Playing adventurer card...\n");
+          while (!isGameOver(&G)) {
+            money = 0;
+            stewardPos = -1;
+            adventurerPos = -1;
+            for (i = 0; i < numHandCards(&G); i++) {
+              if (handCard(i, &G) == copper)
+            money++;
+              else if (handCard(i, &G) == silver)
+            money += 2;
+              else if (handCard(i, &G) == gold)
+            money += 3;
+              else if (handCard(i, &G) == steward)
+            stewardPos = i;
+              else if (handCard(i, &G) == adventurer)
+            adventurerPos = i;
+            }
 
-        //Run adventurer card
-        adventurerCard(&G);
+            if (whoseTurn(&G) == 0) {
+              if (stewardPos != -1) {
+                struct gameState orig;
+                memcpy(&orig, &G, sizeof(struct gameState));
 
-        //Confirm if adventurer card is removed from the hand
-        if(G.hand[0][0] != adventurer)
-            printf("Confirmed removal of adventurer card: PASS\n");
-        else
-            printf("Confirmed removal of adventurer card: FAIL\n");
+                int choice1 = -1;
+                switch(rand() % 3){
+                    case 1:
+                        choice1 = 1; break;
+                    case 2:
+                        choice1 = 2; break;
+                }
 
-        //Check if the total cards in hand is 2
-        if(G.handCount[0] == 2)
-            printf("Confirmed total cards on hand is 2: %d - PASS\n", G.handCount[i]);
-        else
-            printf("Confirmed total cards on hand is 2: %d - FAIL\n", G.handCount[i]);
+                playCard(stewardPos, choice1, -1, -1, &G); 
+                
+                int player = whoseTurn(&G);
 
-        //Increases the count of each coin type chosen
-        for(j = 0; j < 2; j++){
-            if(G.hand[0][j] == copper)
-                numCopper++;
-            if(G.hand[0][j] == silver)
-                numSilver++;
-            if(G.hand[0][j] == gold)
-                numGold++;
-        }
+                if(choice1 == 1){
+                    assert(G.handCount[player] == orig.handCount[player] + 1);
+                }else if(choice1 == 2){
+                    assert(G.coins == orig.coins + 2);
+                    assert(G.handCount[player] == orig.handCount[player] - 1);
+                }else{
+                    assert(G.handCount[player] == orig.handCount[player] - 3);
+                }
+                money = 0;
+                i=0;
+            while(i<numHandCards(&G)){
+              if (handCard(i, &G) == copper){
+                playCard(i, -1, -1, -1, &G);
+                money++;
+              }
+              else if (handCard(i, &G) == silver){
+                playCard(i, -1, -1, -1, &G);
+                money += 2;
+              }
+              else if (handCard(i, &G) == gold){
+                playCard(i, -1, -1, -1, &G);
+                money += 3;
+              }
+              i++;
+            }
+              }
 
-        printf("Position 1 is: %d\n", G.hand[0][0]);
-        printf("Position 2 is: %d\n", G.hand[0][1]);
-        printf("Position 3 is: %d\n", G.hand[0][2]);
+              if (money >= 8) {
+                buyCard(province, &G);
+              }
+              else if (money >= 6) {
+                buyCard(gold, &G);
+              }
+              else if ((money >= 4) && (numStewards < 2)) {
+                buyCard(steward, &G);
+                numStewards++;
+              }
+              else if (money >= 3) {
+                buyCard(silver, &G);
+              }
+
+              endTurn(&G);
+            }
+            else {
+              if (adventurerPos != -1) {
+            playCard(adventurerPos, -1, -1, -1, &G); 
+            money = 0;
+            i=0;
+            while(i<numHandCards(&G)){
+              if (handCard(i, &G) == copper){
+                playCard(i, -1, -1, -1, &G);
+                money++;         
+              }
+              else if (handCard(i, &G) == silver){
+                playCard(i, -1, -1, -1, &G);
+                money += 2;
+              }
+              else if (handCard(i, &G) == gold){
+                playCard(i, -1, -1, -1, &G);
+                money += 3;
+              }
+              i++;
+            }
+              }
+
+              if (money >= 8) {
+                buyCard(province, &G);
+              }
+              else if ((money >= 6) && (numAdventurers < 2)) {
+            buyCard(adventurer, &G);
+            numAdventurers++;
+              }else if (money >= 6){
+                buyCard(gold, &G);
+                }
+              else if (money >= 3){
+                buyCard(silver, &G);
+              }
+              
+              endTurn(&G);      
+            }
+          } // end of While
     }
 
-    printf("\nNumber of bronze coins selected from test: %d\n", numCopper);
-    printf("Number of silver coins selected from test: %d\n", numSilver);
-    printf("Number of gold coins selected from test: %d\n", numGold);
-    printf("\nTesting complete...\n\n");
-
-    return 0;
-
+    printf("ALL TESTS OK\n");
 }
